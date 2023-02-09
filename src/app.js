e3186dea2e247526fd21a6e3711b3865f92a8615
cervino <cervino1999@gmail.com>
2023-02-08 22:04:46 -0300

    Prueba git

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
-//rutas
+//rutass 
 app.use(express.json());
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartsRouter);
