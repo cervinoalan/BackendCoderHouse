1545f652f2d0fca61f50703e9ee4a6439d789c16
cervino <cervino1999@gmail.com>
2023-03-05 20:58:00 -0300

    agregando session controller

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,36 +1,11 @@
 const { Router } = require('express');
-const UsersModel = require('../dao/models/user.model');
-
 const router = Router();
+const sessionController = require("../controller/session.controller")
 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await UsersModel.findOne({ email, password });
-  if (user) {
-    req.session.user = user;
-    res.send(user);
-  } else {
-    res.status(401).send('Email o contraseña incorrectos');
-  }
-});
+router.post('/login', sessionController.login);
 
-router.post('/register', async (req, res) => {
-  try {
-    const user = await UsersModel.create(req.body);
-    res.send(user);
-  } catch (error) {
-    res.status(500).send('Error al crear usuario');
-  }
-});
+router.post('/register', sessionController.register);
 
-router.post("/logout", (req, res) => {
-  req.session.destroy((error) => {
-    if (!error) {
-      res.send("Sesion cerrada con exito!");
-    } else {
-      res.status(500).send("Ocurrio un error el cerrar sesion")
-    }
-  });
-});
+router.post("/logout", sessionController.logout);
 
 module.exports = router;
